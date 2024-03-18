9eb986cd1219a4d376404340968eef5c8750d55c
Uzeyir Mammadli <uzeyirmammadlee@gmail.com>
2024-03-18 16:20:13 +0400

    update choices

--- example.py
+++ example.py
@@ -1,8 +1,14 @@
 print("1. Report case\n2. Scan area for cats\n3. Resolve case")
 command = input("What would you like to do?")
 
-result = f'You chose {command}'
-
+if command == "1":
+    result = f'You chose reporting case'
+elif command == "2":
+    result = f'You chose scanning area for cats'
+elif command == "3":
+    result = f'You chose resolving case'
+else:
+    result = f'Your command is unknown'
 print(result)
 
 
