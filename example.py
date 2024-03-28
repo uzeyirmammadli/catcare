e88b30b3e49d891833980ce7389ec96ddbe17eb3
Uzeyir Mammadli <uzeyirmammadlee@gmail.com>
2024-03-28 16:21:58 +0400

    datetime create_at

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 import random
 import csv
 import os
+from datetime import datetime
 
 
 #DATA
@@ -13,12 +14,16 @@ def initialize(db):
   return os.path.isfile(db)
 
 def create_case(db, report):
-  fieldnames = [
-      'id', 'photo', 'location', 'need', 'status', 'created_at', 'updated_at'
-  ]
-  with open(db, 'a') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writerow(report)
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    report['created_at'] = current_time
+    report['updated_at'] = current_time
+    
+    fieldnames = [
+        'id', 'photo', 'location', 'need', 'status', 'created_at', 'updated_at'
+    ]
+    with open(db, 'a') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(report)
 
 def update_case(db,id,updated_case):
   data = []
@@ -92,8 +97,26 @@ def report_inputs():
   need = input("What should be done?\t")
   return {'photo': photo, 'area': area, 'need': need}
 
+def scan_inputs():
+    area   = input("An area name to tag\t")
+    return area
+
+def resolve_inputs():
+    case_id = input("Case id to resolve\t")
+    return case_id
 
 
+#Entity
+def build_report(r):
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return {
+        'id': f'{random.random()}',
+        'photo': r['photo'],
+        'location': r['area'],
+        'need': r['need'],
+        'status': 'OPEN',
+        'created_at': current_time
+    }    
 
 
 #BOOT
@@ -102,28 +125,23 @@ x = input('Do you want to seed the database? (y/n)')
 if x == 'y':
   seed()
 
-#PROGRM
+#PROGRAM
 while True:
   print("1. Report case\n2. Scan area for cats\n3. Resolve case")
   command = input("What would you like to do?")
   if command == "1":
     print('You chose reporting case')
+    
     r = report_inputs()
-    report = {
-        'id': f'{random.random()}',
-        'photo': r['photo'],
-        'location': r['area'],
-        'need': r['need'],
-        'status': 'OPEN'
-    }
+    report = build_report(r)
     create_case('cats.csv', report)
   elif command == "2":
     print('You chose scanning area for cats')
-    area = input("Area ->")
+    area = scan_inputs()
     found = scan_case('cats.csv', area)
     present(found)
   elif command == "3":
-    case_id = input("ID -> ")
+    case_id = resolve_inputs()
     result = resolve_case('cats.csv', case_id)
     present([result])
   else:
